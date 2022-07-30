3d22013542243ca2f00b08fc0267fc3d138bbcf2
ibarbenza <ibarbenza@gmail.com>
2022-07-30 19:52:18 -0300

    Version 0.0.3 - Vista inicio creada y renderizada

--- MVT_Project/MVT_App/models.py
+++ MVT_Project/MVT_App/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Familia(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
-    fecha_nac = models.DateField()
+    fecha_nac = models.CharField(max_length=50)
     dni = models.IntegerField()
--- MVT_Project/MVT_App/views.py
+++ MVT_Project/MVT_App/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from MVT_App.models import Familia
 
 # Create your views here.
+
+def familia(request):
+    flia = Familia.objects.all()
+    return render(request, "MVT_App/index.html", {"familia" : flia})
